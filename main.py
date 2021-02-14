7a5940cac91f82edb55ee294b031c72e9ed46cd5
Ian McKernan <itmmckernan@gmail.com>
2021-02-14 02:09:00 -0800

    reeeeeeeeeeeeeeeeeeeeeeeeee

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ app = dash.Dash(__name__)
 #test for a commit
 
 powerPlants = pd.read_csv(open('./dataFiles/global_power_plant_database.csv', encoding='utf-8'))
-cancerRates = pd.read_csv(open('dataFiles/incd_lung.csv', encoding='utf-8'))
+cancerRates = pd.read_csv(open('dataFiles/incd_all.csv', encoding='utf-8'))
 countyCenters = pd.read_csv(open('./dataFiles/county_centers.csv', encoding='utf-8'))
 
 
-cancerMerged = pd.merge(cancerRates, countyCenters, on="fips", how='left')
+cancerMerged = cancerRates.merge(countyCenters, on="fips", how='right')
 powerPlants = powerPlants.query("primary_fuel == 'Coal'")
 tree = KDTree(powerPlants[['latitude', 'longitude']].values)
 
@@ -24,9 +24,14 @@ figure = go.Figure()
 pollutionRateByCounty = [0] * len(cancerMerged)
 print(cancerMerged)
 for county in cancerMerged.iterrows():
-
-    treeResults = tree.query(x=[county[1]['pclon10'], county[1]['pclat10']], k=50)
+    if math.isnan(county[1]['pclon10']):
+        coords = [county[1]['pclon00'], county[1]['pclat00']]
+    else:
+        coords = [county[1]['pclon10'], county[1]['pclat10']]
+    treeResults = tree.query(x=coords, k=150)
     for treeResult in zip(treeResults[0], treeResults[1]):
+        if math.isinf(treeResult[0]):
+            continue
         try:
             result = powerPlants.iloc[treeResult[1]]
         except IndexError:
@@ -36,7 +41,7 @@ for county in cancerMerged.iterrows():
 
 cancerMerged.insert(6, 'pollutionScore', pollutionRateByCounty)
 
-chart1 = px.scatter(data_frame=cancerMerged, x='pollutionScore', y='ratePer100k', trendline="ols")
-chart2 = px.scatter_geo(cancerMerged, lat='pclat10', lon='pclon10', size='pollutionScore')
+chart1 = px.scatter(data_frame=cancerMerged, x='pollutionScore', y='ratePer100k', trendline="ols", hover_name='State')
+chart2 = px.scatter_geo(cancerMerged, lat='pclat10', lon='pclon10', size='pollutionScore', hover_name='State')
 chart1.show()
 chart2.show()
