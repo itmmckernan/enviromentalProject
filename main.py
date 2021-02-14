bab1e0dde2aa950127643071497943efe75b0dc6
Ian McKernan <itmmckernan@gmail.com>
2021-02-14 14:17:53 -0800

    made fastererr

--- main.py
+++ main.py
@@ -1,47 +1,59 @@
 import pandas as pd
 from scipy.spatial import KDTree
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output
 import plotly.express as px
-import plotly.graph_objects as go
 import math
 
-app = dash.Dash(__name__)
 #test for a commit
 
 powerPlants = pd.read_csv(open('./dataFiles/global_power_plant_database.csv', encoding='utf-8'))
 cancerRates = pd.read_csv(open('dataFiles/incd_all.csv', encoding='utf-8'))
 countyCenters = pd.read_csv(open('./dataFiles/county_centers.csv', encoding='utf-8'))
 
-
+plantTypes = ['Wind', 'Coal', 'Gas', 'Hydro', 'Oil', 'Biomass', 'Waste']
 cancerMerged = cancerRates.merge(countyCenters, on="fips", how='right')
-powerPlants = powerPlants.query("primary_fuel == 'Coal'")
-tree = KDTree(powerPlants[['latitude', 'longitude']].values)
+pollutionRateByCounty = {}
+subPowerPlants = {}
+subTrees = {}
+for plantType in plantTypes:
+    subPowerPlants[plantType] = powerPlants.query("primary_fuel == '{}'".format(plantType))
+    subTrees[plantType] = KDTree(subPowerPlants[plantType][['latitude', 'longitude']].values)
 
-figure = go.Figure()
-pollutionRateByCounty = [0] * len(cancerMerged)
-print(cancerMerged)
 for county in cancerMerged.iterrows():
+    print(county[0])
     if math.isnan(county[1]['pclon10']):
         coords = [county[1]['pclon00'], county[1]['pclat00']]
     else:
         coords = [county[1]['pclon10'], county[1]['pclat10']]
-    treeResults = tree.query(x=coords, k=150)
-    for treeResult in zip(treeResults[0], treeResults[1]):
-        if math.isinf(treeResult[0]):
-            continue
-        try:
-            result = powerPlants.iloc[treeResult[1]]
-        except IndexError:
-            print([county[1]['pclon10'], county[1]['pclat10']])
-            print(county)
-        pollutionRateByCounty[county[0]] += result['capacity_mw'] / math.pow(treeResult[0], 2)
+    pollutionRateByCounty[county[1]['fips']] = {}
+    for plantType in plantTypes:
+        treeResults = subTrees[plantType].query(x=coords, k=5)
+        pollutionRateByCounty[county[1]['fips']][plantType] = 0
+        for treeResult in zip(treeResults[0], treeResults[1]):
+            if math.isinf(treeResult[0]):
+                print("skipped due to inf")
+                continue
+            try:
+                result = subPowerPlants[plantType].iloc[treeResult[1]]
+            except IndexError:
+                print('countyError')
+            pollutionRateByCounty[county[1]['fips']][plantType] += result['capacity_mw'] / math.pow(treeResult[0], 2)
+print(pollutionRateByCounty)
+pollutionRateDataframe = pd.DataFrame.from_dict(pollutionRateByCounty).transpose()
+pollutionRateDataframe['fips'] = pollutionRateDataframe.index
+print(pollutionRateDataframe)
+cancerMerged = cancerRates.merge(pollutionRateDataframe, on="fips")
 
-cancerMerged.insert(6, 'pollutionScore', pollutionRateByCounty)
+figure = px.scatter_geo(cancerMerged, lat='pclat10', lon='pclon10', size='Coal', hover_name='State')
+for plantType in plantTypes:
+    figure.add_trace(
+        px.scatter(data_frame=cancerMerged,
+                   x=plantType,
+                   y='ratePer100k',
+                   trendline="ols",
+                   color=px.Constant(plantType),
+                   title=plantType
+                   )
+    )
 
-chart1 = px.scatter(data_frame=cancerMerged, x='pollutionScore', y='ratePer100k', trendline="ols", hover_name='State')
-chart2 = px.scatter_geo(cancerMerged, lat='pclat10', lon='pclon10', size='pollutionScore', hover_name='State')
-chart1.show()
-chart2.show()
+
+figure.show()
