15c686b9f2feedcc98fe43028cc80cf10316a23a
Ian McKernan <itmmckernan@gmail.com>
2021-02-18 08:10:20 -0800

    unrolling loops and such for speeeeeeeeeeeed

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import plotly.express as px
 import plotly.subplots as sp
 import math
 
-#test for a commit
+# test for a commit
 
 powerPlants = pd.read_csv(open('./dataFiles/global_power_plant_database.csv', encoding='utf-8'))
 cancerRates = pd.read_csv(open('dataFiles/incd_all.csv', encoding='utf-8'))
@@ -30,26 +30,39 @@ for county in cancerMerged.iterrows():
         treeResults = subTrees[plantType].query(x=coords, k=15)
         pollutionRateByCounty[county[1]['fips']][plantType] = 0
         for treeResult in zip(treeResults[0], treeResults[1]):
-            if math.isinf(treeResult[0]):
-                print("skipped due to inf")
-                continue
-            try:
-                result = subPowerPlants[plantType].iloc[treeResult[1]]
-            except IndexError:
-                print('countyError')
+            result = subPowerPlants[plantType].iloc[treeResult[1]]
             pollutionRateByCounty[county[1]['fips']][plantType] += result['capacity_mw'] / math.pow(treeResult[0], 2)
+
+#make coords
+cancerMerged['coords'] = [cancerMerged['pclon00'], cancerMerged['pclat00']] if math.isnan(cancerMerged['pclat10']) else [cancerMerged['pclon10'], cancerMerged['pclat10']]
+#query tree
+for plantType in plantTypes:
+    cancerMerged[plantType+'_tree'] = zip(subTrees[plantType].query(x=cancerMerged['coords'], k=50))
+
+#look at points in vecotr magical way
+
+
 pollutionRateDataframe = pd.DataFrame.from_dict(pollutionRateByCounty).transpose()
 pollutionRateDataframe['fips'] = pollutionRateDataframe.index
-cancerMerged = cancerRates.merge(pollutionRateDataframe, on="fips")
-print(cancerMerged)
-#figure = px.scatter_geo(cancerMerged, lat='pclat10', lon='pclon10', size='Coal', hover_name='State')
+fullData = cancerMerged.merge(pollutionRateDataframe, on="fips")
+print(fullData)
+
+subplotFigure = sp.make_subplots(rows=6, cols=1)
+subplotFigure.add_trace(
+    px.scatter_geo(fullData, lat='pclat10', lon='pclon10', size='Coal', hover_name='State'),
+    row=1,
+    col=1)
+iterNumber = 1
 for plantType in plantTypes:
-        px.scatter(data_frame=cancerMerged,
+    iterNumber += 1
+    subplotFigure.add_trace(
+        px.scatter(data_frame=fullData,
                    x=plantType,
                    y='ratePer100k',
                    trendline="ols",
                    color=px.Constant(plantType),
-                   title=plantType
-                   ).show()
-
-
+                   title=plantType,
+                   ),
+        row=iterNumber,
+        col=1
+    )
