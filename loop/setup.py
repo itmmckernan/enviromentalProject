8ed41be13a7f49d323bfc64c75d0f6fa9ff9d594
itmmckernan <itmmckernan@gmail.com>
2021-03-28 19:44:30 -0700

    final data stuff

--- loop/setup.py
+++
@@ -1,6 +0,0 @@
-import os
-from setuptools import setup, Extension
-
-module = Extension('cancerMerged', sources=['cancerMerged.cpp'], language='c++')
-
-setup(name='cancerMerged', ext_modules = [module])
