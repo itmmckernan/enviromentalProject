2d713fc9040a49753e3da0e526831931f3388d79
itmmckernan <itmmckernan@gmail.com>
2021-03-28 22:43:39 -0700

    why github pages, why

--- main.py
+++ main.py
@@ -9,6 +9,17 @@ cancerRates = pd.read_csv(open('dataFiles/incd_lung.csv', encoding='utf-8'))
 countyCenters = pd.read_csv(open('./dataFiles/county_centers.csv', encoding='utf-8'))
 
 plantTypes = ['Nuclear', 'Wind', 'Solar', 'Coal', 'Gas', 'Hydro', 'Oil', 'Biomass', 'Waste']
+plantRenewable = {
+    'Nuclear': True,
+    'Wind': True,
+    'Solar': True,
+    'Coal': False,
+    'Gas': False,
+    'Hydro': False,
+    'Oil': False,
+    'Biomass': False, #only kind of lol,for the purposes of pollution yes
+    'Waste': True
+}
 # merge cancerRates and countyCenters datasets together
 cancerMerged = cancerRates.merge(countyCenters, on="fips", how='left')
 # setup dicts
@@ -41,7 +52,7 @@ for plantType in plantTypes:
     print(plantType + " done")
 
 cancerMerged['State'] = cancerMerged['State'].str.slice(stop=-3)
-cancerMerged = cancerMerged.rename(columns={
+newNameDict = {
     "WindScore": "Wind Proximity Value",
     "SolarScore": "Solar Proximity Value",
     "CoalScore": "Coal Proximity Value",
@@ -51,7 +62,8 @@ cancerMerged = cancerMerged.rename(columns={
     "BiomassScore": "Biomass Proximity Value",
     "WasteScore": "Waste Proximity Value",
     "NuclearScore": "Nuclear Proximity Value"
-})
+}
+cancerMerged = cancerMerged.rename(columns=newNameDict)
 # px.scatter_geo(cancerMerged, lat='pclat10', lon='pclon10', size=["Wind Proximity Value", "Coal Proximity Value", "Gas Proximity Value", "Hydroelectric Proximity Value", "Oil Proximity Value", "Biomass Proximity Value", "Waste Proximity Value"], hover_name='State').show()
 
 figure = px.scatter(data_frame=cancerMerged,
@@ -61,14 +73,23 @@ figure = px.scatter(data_frame=cancerMerged,
                        "Waste Proximity Value", "Nuclear Proximity Value"],
                     y='ratePer100k',
                     trendline="ols",
-                    title='Cancer Rates Per 100k residents versus their proximity to a power plant',
+                    title='Cancer Rates Per 100k residents versus proximity to a power plant',
                     labels={
                         "x": "Proximity Value",
                         "ratePer100k": "Cancer Rate Per 100,000 people, age adjusted"
                     }
                     )
+figure.update_layout(xaxis_title = "Proximity Score",
+                     legend_title_text="Power Plant Type",
+                     legend_traceorder='grouped',
+                     legend_tracegroupgap=10)
+#for trace in figure.data:
+#   if plantRenewable[list(newNameDict.keys())[list(newNameDict.values()).index(trace.name)][:-5]]: #someone should take python away from me for this monstrosity
+#       trace.legendgroup = 'Renewable'
+#   else:
+#       trace.legendgroup = 'Non-Renewable'
 figure.show()
-figure.write_html("dataOutput/scatter.html")
+figure.write_html("scatter.html")
 cancerMerged.to_csv('dataOutput/full.csv')
 for item in ['fips', 'clon00', 'clat00', 'clon10', 'clat10', 'pclon00', 'pclat00', 'pclon10', 'pclat10',
                   'NuclearQuery', 'WindQuery', 'SolarQuery', 'CoalQuery', 'GasQuery', 'HydroQuery', 'OilQuery',
